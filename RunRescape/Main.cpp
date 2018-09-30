13897196c7ad75b9081afb0977fff6155c0dc500
DESKTOP-96R97F0\Brad <Brad.Ragazinsky@gmail.com>
2018-09-30 14:14:11 -0400

    Added window logic

---
+++ RunRescape/Main.cpp
@@ -0,0 +1,24 @@
+#include <glad/glad.h>
+#include <GLFW/glfw3.h>
+#include <iostream>
+using namespace std;
+
+int main() {
+
+	glfwInit();
+	glfwWindowHint(GLFW_CONTEXT_VERSION_MAJOR, 3);
+	glfwWindowHint(GLFW_CONTEXT_VERSION_MAJOR, 3);
+	glfwWindowHint(GLFW_OPENGL_PROFILE, GLFW_OPENGL_CORE_PROFILE);
+
+	GLFWwindow* window = glfwCreateWindow(800, 600, "RunRescape", NULL, NULL);
+	if (window == NULL) {
+
+		std::cout << "Failed to create GLFW window" << std::endl;
+		glfwTerminate();
+		return -1;
+	}
+
+	glfwMakeContextCurrent(window);
+	return 0;
+
+}
